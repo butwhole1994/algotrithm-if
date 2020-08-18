b9724b639f67ddd74600fe9009b10e9eb742a8d8
butwhole1994 <61606289+butwhole1994@users.noreply.github.com>
2020-08-18 21:52:11 +0900

    Update alarm.java

--- alarm.java
+++ alarm.java
@@ -11,7 +11,8 @@ public class alarm {
 		int h = a;
 		int m = b-45;
 		
-		if(m<0) {
+		if(m<0) { // --나 += 등의 쓰임새를 기억해두자.a--는 1 뺀 값이 변수a가 된다.  a++는 1 더한 값이 a가 된다.
+					// a-=b 나 a+=b의 경우 a,b를 더하거나 뺀 숫자가 a가 된다는 의미. 
 			h--;
 			m += 60;
 			if(h<0) {
@@ -19,6 +20,7 @@ public class alarm {
 			}
 		}
 		System.out.println(h+" "+m);
+		//모든 답이 같은 형식으로 나오게 해서 sout의 횟수를 줄인다.
 	}
 
 }
